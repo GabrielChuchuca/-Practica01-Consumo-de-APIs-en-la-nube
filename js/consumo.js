9e6e6653e334b40bcd935e9e9684e583500051b8
GabrielChuchuca <gavriellalx@hotmail.com>
2021-04-11 00:39:38 -0500

    Guia de Practica

--- js/consumo.js
+++ js/consumo.js
@@ -6,7 +6,19 @@ function consumoApi(){
     request.onload = function () {
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
-
-	}
+        for(var i=0; i < data['articles'].length; i++){
+            detalles += "<tr>" +
+            "<td>" + data['articles'][i]['author'] + "</td>" +
+            "<td>" + data['articles'][i]['title'] + "</td>" +
+            "<td>" + data['articles'][i]['description'] + "</td>" +
+            "<td><a href=" + data['articles'][i]['url'] + " target='_blank'>link</a></td>" +
+            "<td><img src=" + data['articles'][i]['urlToImage'] + " width='500' height='400'></td>" +
+            "<td>" + data['articles'][i]['publishedAt'] + "</td>" +
+            "<td>" + data['articles'][i]['content'] + "</td>" +
+            "</tr>"
+        }
+        document.getElementById("tablaDATOS").innerHTML = detalles
+        }
+    }
     request.send();
 }
